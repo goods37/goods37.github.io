cd9449c0385ace9d39e21f0e3ca070e6c52a6c38
Eric Goodman <ericgoodman37@gmail.com>
2017-08-03 21:29:04 +0300

    followed artists page nearly complete, recently played tracks functioning but bad ui

--- js/app.js
+++ js/app.js
@@ -2,31 +2,90 @@
  * Created by Eric Goodman on 6/17/17.
  */
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute'])
+    .service('spotify', function ($http, $q) {
 
-//configure our routes
-// app.config(function ($routeProvider) {
-//     $routeProvider
-//         .when('/', {
-//             templateUrl: '../../spotifyme/src/public/followed.html',
-//             controller: 'followedArtists'
-//         })
-//         .when('/recent', {
-//             templateUrl: 'app',
-//             controller: 'contactController'
-//         });
-// });
+        var cache;
 
-app.controller('followed', function mainController($scope, $http) {
+        function getSpotifyData() {
+            var q = $q.defer();
+            if(cache) {
+                q.resolve(cache);
+            }
+            $http.get('https://spotifyeric.herokuapp.com/')
+                .then(function success(response) {
+                        cache = response.data;
+                        q.resolve(cache)
+                    },
+                    function failure(err) {
+                        q.reject(err);
+                    }
+                );
+            return q.promise;
+        }
 
-    $http.get('https://spotifyeric.herokuapp.com/')
-        .then(function (response) {
-            var data = response.data;
-            $scope.followedArtists = data[0].followed_artists;
-            console.log($scope.followedArtists);
-            console.log($scope.followedArtists[0].popularity);
-        }).catch(function (err) {
-        return console.error(err);
+        return {getSpotifyData : getSpotifyData};
     });
 
+
+//configure our routes
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/recent', {
+            templateUrl: 'recent.html',
+            controller: 'recent'
+        })
+        .when('/followed', {
+            templateUrl: 'followed.html',
+            controller: 'followed'
+        })
+        .otherwise({
+            controller: 'mainController'
+        });
+});
+
+
+app.controller('mainController', function ($scope, $location, spotify) {
+    // Get data from Spotify, then present user with followed artists page
+
+    $scope.loadFollowed = function() {
+        $location.url('/followed');
+    };
+
+    $scope.loadRecent = function() {
+        $location.url('/recent');
+    };
+
+    spotify.getSpotifyData().then( function(response) {
+        $location.url('/followed')
+    });
+});
+
+app.controller('followed', function followed($scope, spotify) {
+    spotify.getSpotifyData().then(function (response) {
+            $scope.followedArtists = response[0].followed_artists;
+
+            var spinner = $('.spinner');
+            spinner.css('visibility', 'hidden');
+            spinner.remove();
+
+            $('#main').css({
+                opacity: 0.0,
+                visibility: "visible"
+            }).animate({opacity: 1.0}, 2000);
+
+        }).catch(function (err) {
+
+        console.error(err);
+    });
+});
+
+app.controller('recent', function recent($scope, spotify) {
+    spotify.getSpotifyData().then(function(response) {
+       $scope.recentlyPlayed = [];
+       response[1].recently_played.items.forEach(function(item) {
+           $scope.recentlyPlayed.push(item.track);
+       });
+        console.log($scope.recentlyPlayed);
+    });
 });
