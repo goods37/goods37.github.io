abad3283897256d60a2015f9179d110829f39118
Eric Goodman <ericgoodman37@gmail.com>
2017-08-04 19:49:00 +0300

    essentially working versions of spotifyme frontend

--- js/app.js
+++ js/app.js
@@ -2,31 +2,7 @@
  * Created by Eric Goodman on 6/17/17.
  */
 
-var app = angular.module('app', ['ngRoute'])
-    .service('spotify', function ($http, $q) {
-
-        var cache;
-
-        function getSpotifyData() {
-            var q = $q.defer();
-            if(cache) {
-                q.resolve(cache);
-            }
-            $http.get('https://spotifyeric.herokuapp.com/')
-                .then(function success(response) {
-                        cache = response.data;
-                        q.resolve(cache)
-                    },
-                    function failure(err) {
-                        q.reject(err);
-                    }
-                );
-            return q.promise;
-        }
-
-        return {getSpotifyData : getSpotifyData};
-    });
-
+var app = angular.module('app', ['ngRoute']);
 
 //configure our routes
 app.config(function ($routeProvider) {
@@ -45,47 +21,77 @@ app.config(function ($routeProvider) {
 });
 
 
+app.service('spotify', function ($http, $q) {
+
+    function getSpotifyData() {
+        var q = $q.defer();
+        $http.get('https://spotifyeric.herokuapp.com/', {cache: true})
+            .then(function success(response) {
+                    cache = response.data;
+                    q.resolve(cache)
+                },
+                function failure(err) {
+                    q.reject(err);
+                }
+            );
+        return q.promise;
+    }
+
+    return {getSpotifyData: getSpotifyData};
+});
+
+
 app.controller('mainController', function ($scope, $location, spotify) {
     // Get data from Spotify, then present user with followed artists page
 
-    $scope.loadFollowed = function() {
+    $scope.loadFollowed = function () {
+        $scope.title = 'Followed Artists';
         $location.url('/followed');
     };
 
-    $scope.loadRecent = function() {
+    $scope.loadRecent = function () {
+        $scope.title = 'Recently Played Tracks';
         $location.url('/recent');
     };
 
-    spotify.getSpotifyData().then( function(response) {
-        $location.url('/followed')
+    // Default to followed artists
+    spotify.getSpotifyData().then(function (res) {
+        $scope.title = 'Followed Artists';
+        $location.url('/followed');
     });
+
+
 });
 
 app.controller('followed', function followed($scope, spotify) {
     spotify.getSpotifyData().then(function (response) {
-            $scope.followedArtists = response[0].followed_artists;
+        $scope.followedArtists = response[0].followed_artists;
 
-            var spinner = $('.spinner');
-            spinner.css('visibility', 'hidden');
-            spinner.remove();
+        var spinner = $('.spinner');
+        spinner.css('visibility', 'hidden');
+        spinner.remove();
 
-            $('#main').css({
-                opacity: 0.0,
-                visibility: "visible"
-            }).animate({opacity: 1.0}, 2000);
+        $('#main').css({
+            opacity: 0.0,
+            visibility: "visible"
+        }).animate({opacity: 1.0}, 2000);
 
-        }).catch(function (err) {
+    }).catch(function (err) {
 
         console.error(err);
     });
 });
 
 app.controller('recent', function recent($scope, spotify) {
-    spotify.getSpotifyData().then(function(response) {
-       $scope.recentlyPlayed = [];
-       response[1].recently_played.items.forEach(function(item) {
-           $scope.recentlyPlayed.push(item.track);
-       });
+    spotify.getSpotifyData().then(function (response) {
+        $scope.recentlyPlayed = [];
+        response[1].recently_played.items.forEach(function (item) {
+            $scope.recentlyPlayed.push(item.track);
+        });
+        $('#main').css({
+            opacity: 0.0,
+            visibility: "visible"
+        }).animate({opacity: 1.0}, 2000);
         console.log($scope.recentlyPlayed);
     });
 });
